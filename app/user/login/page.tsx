534c941208932292e315e79521814e0f3deb2d2c
MikiyaYamaguchi <mikiyagreeeen@gmail.com>
2025-04-07 23:17:14 +0900

    ログインページを実装

--- app/user/login/page.tsx
+++ app/user/login/page.tsx
@@ -1,5 +1,74 @@
+"use client";
+import React, { ChangeEvent, useState } from "react";
+
+interface User {
+  email: string;
+  password: string;
+}
+
 const Login = () => {
-  return <h1>ログインページ</h1>;
+  const [user, setUser] = useState<User>({ email: "", password: "" });
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:3000/api/user/login", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
+      const jsonData = await response.json();
+      localStorage.setItem("token", jsonData.token);
+      alert(jsonData.message);
+    } catch {
+      alert("ログイン失敗");
+    }
+  };
+  return (
+    <div>
+      <h1>ログイン</h1>
+      <form onSubmit={handleSubmit}>
+        <table>
+          <tbody>
+            <tr>
+              <th>メールアドレス</th>
+              <td>
+                <input
+                  type="email"
+                  name="email"
+                  value={user.email}
+                  onChange={handleChange}
+                  placeholder="xxx@xxx.com"
+                  required
+                />
+              </td>
+            </tr>
+            <tr>
+              <th>パスワード</th>
+              <td>
+                <input
+                  type="text"
+                  name="password"
+                  value={user.password}
+                  onChange={handleChange}
+                  required
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <button>ログイン</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
