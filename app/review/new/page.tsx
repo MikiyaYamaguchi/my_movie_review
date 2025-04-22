c051c50d9cd0036344534ed9b1b6be7bfa8534b9
MikiyaYamaguchi <mikiyagreeeen@gmail.com>
2025-04-22 21:25:21 +0900

    新着レビューページ作成

---
+++ app/review/new/page.tsx
@@ -0,0 +1,27 @@
+import Card from "@/app/components/card";
+import { getAllReviews } from "@/app/lib/review";
+
+interface Review {
+  _id: string;
+  title: string;
+  date: Date;
+  genre: string;
+  image: string;
+  star: number;
+  thoughts: string;
+}
+
+const New = async () => {
+  const allReviews = await getAllReviews();
+  const listReviews = allReviews.map((review: Review) => {
+    return <Card review={review} key={review._id} />;
+  });
+  return (
+    <>
+      <h2>新着レビュー</h2>
+      <div className="row sp-col-2">{listReviews}</div>
+    </>
+  );
+};
+
+export default New;
